63ea97f732528533e46eb286ed181545337e3a8d
dbradley-xi <96792909+dbradley-xi@users.noreply.github.com>
2023-01-29 19:38:26 +0000

    Updated folders and locations

---
+++ Example Commands to update resources/AddMemberToTeam.ps1
@@ -0,0 +1,10 @@
+#####
+# Author: Daniel Bradley
+# LinkedIn: https://www.linkedin.com/in/danielbradley2/
+# Twitter: https://twitter.com/DanielatOCN
+# Website: https://ourcloudnetwork.com/
+# Info: This script was written by Daniel Bradley for the Microsoft Graph PowerShell Zero to Hero Book
+#####
+
+Import-Module Microsoft.Graph.Teams
+Connect-MgGraph
