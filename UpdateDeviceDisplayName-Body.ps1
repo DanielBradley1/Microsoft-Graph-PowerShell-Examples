747f0cb5f5ec1c943b5bdf347cfe52d9eaf71dff
dbradley-xi <96792909+dbradley-xi@users.noreply.github.com>
2023-01-29 19:16:45 +0000

    Updated

--- UpdateDeviceDisplayName-Body.ps1
+++ UpdateDeviceDisplayName-Body.ps1
@@ -1,3 +1,11 @@
+#####
+# Author: Daniel Bradley
+# LinkedIn: https://www.linkedin.com/in/danielbradley2/
+# Twitter: https://twitter.com/DanielatOCN
+# Website: https://ourcloudnetwork.com/
+# Info: This script was written by Daniel Bradley for the Microsoft Graph PowerShell Zero to Hero Book
+#####
+
 Import-Module Microsoft.Graph.Identity.DirectoryManagement
 Connect-MgGraph Device.ReadWrite.All
 
@@ -5,4 +13,5 @@ $device = Get-MgDevice -Search "displayName:TargetDevice" -ConsistencyLevel even
 $params = @{
 	DeviceName = MyNewDisplayName
 }
+
 Update-MgDevice -DeviceId $device.id -BodyParameter $params
