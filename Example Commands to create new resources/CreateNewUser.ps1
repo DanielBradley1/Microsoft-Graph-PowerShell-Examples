738a6a775f634643498bbc2cfdf258f6f1f0d794
dbradley-xi <96792909+dbradley-xi@users.noreply.github.com>
2023-02-02 06:47:35 +0000

    Upload new files

---
+++ Example Commands to create new resources/CreateNewUser.ps1
@@ -0,0 +1,25 @@
+#####
+# Author: Daniel Bradley
+# LinkedIn: https://www.linkedin.com/in/danielbradley2/
+# Twitter: https://twitter.com/DanielatOCN
+# Website: https://ourcloudnetwork.com/
+# Info: This script was written by Daniel Bradley for the Microsoft Graph PowerShell Zero to Hero Book
+#####
+
+##Import the Microsoft Graph Users module
+Import-Module Microsoft.Graph.Users
+
+##Connect to Microsoft Graph with the user read/write permission
+Connect-MgGraph -scope User.ReadWrite.All
+
+##Define the password profile settings within a hash table
+$PasswordProfile = @{
+    Password = "Helo123!"
+    ForceChangePasswordNextSignIn = $true
+    ForceChangePasswordNextSignInWithMfa = $true
+}
+
+##Create the new user account
+New-MgUser -DisplayName "New User" -PasswordProfile $PassProfile `
+-AccountEnabled -MailNickName "NewUser" `
+-UserPrincipalName "newuser@ourcloudnetwork.com"
