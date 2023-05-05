cb1e0debf8765f2879f0cc91fe21827acc3a97ab
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2023-05-05 11:51:24 +0100

    Update Get-LapsAADPassword.ps1

--- Get-LapsAADPassword.ps1
+++ Get-LapsAADPassword.ps1
@@ -1,3 +1,22 @@
+<#
+AUTHOR: Daniel Bradley
+LINKEDIN: https://www.linkedin.com/in/danielbradley2/
+TWITTER: https://twitter.com/DanielatOCN
+WEBSITE: https://ourcloudnetwork.com/
+Info: This script was written by Daniel Bradley for the ourcloudnetwork.com blog
+#>
+
+####### Script Notes ######
+# This script is designed to use the Invoke-MgGraphRequest cmdlet to retrieve the current LAPS password from Azure AD for single specific device.
+# Example: Get-LapsAADPassword.ps1 -DeviceName Win11Desktop02
+###########################
+
+###### Parameters ######
+[cmdletbinding()]
+param
+(
+    [string]$DeviceName
+    )
 
 Write-Host "Checking for Microsoft Graph Modules" -ForegroundColor Cyan
 #Install MS Graph if not available
@@ -13,29 +32,36 @@ else {
 #Connect to Microsoft Graph
 Write-host "Connecting to Microsoft Graph" -ForegroundColor Cyan
 Connect-Mggraph -Scope DeviceLocalCredential.Read.All, Device.Read.All
+Write-host "Connected"
 
-#Checking Connection
-Function Check-MgContext
-{
-    if ((Get-MgContext).Scopes -match "Device") {
-        Continue
-    } 
-    else {
-        Do {
-        Write-Host "Session not established, retrying authentication..." -ForegroundColor Yellow
-        Connect-Mggraph -Scope DeviceLocalCredential.Read.All, Device.Read.All
-        } While ((Get-MgContext).Scopes -eq $null)
-        Write-Host "Session established" -ForegroundColor Green
-    }
+Write-host "Checking Session" -ForegroundColor Yellow
+if ((Get-MgContext).Scopes -match "Device") {}
+else {
+    Do {
+    Write-Host "Session not established, retrying authentication..." -ForegroundColor Yellow
+    Connect-Mggraph -Scope DeviceLocalCredential.Read.All, Device.Read.All
+    $var++
+    Write-host "$var"
+    } Until ((Get-MgContext).Scopes -ne $null -or $var -eq 3)
+	If ((Get-MgContext).Scopes -match "Device") {
+		Write-host "Connection successful" -foregroundcolor Green
+	} else {
+		Write-host "Connection not successful, exiting script." -foregroundcolor Yellow
+		return
+	}
 }
 
-#Run Function
-Check-MgContext
+Write-Host "Looking for $DeviceName"
 
 #Define your device name here
-$DeviceName = "Win11Desktop02"
+#$DeviceName = "Win11Desktop02"
 #Store the device id value for your target device
 $DeviceId = (Get-MgDevice | Where-Object {$_.DisplayName -eq $DeviceName} | Select DeviceId).DeviceId
+
+If ($DeviceId -eq $null){
+    Write-host "Device not found! Exiting script." -ForegroundColor Yellow
+    Return
+}
 
 #Generate a new correlation ID
 $correlationID = [System.Guid]::NewGuid()
@@ -55,4 +81,7 @@ $Response = Invoke-MgGraphRequest -Method GET -Uri $URI -Headers $headers
 
 #Decode the LAPS password from Base64
 $passwordb64 = ($Response.credentials).PasswordBase64
-$passwordb64 | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))}
+$password = $passwordb64 | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))}
+Write-host "$Password"
+
+Disconnect-MgGraph
