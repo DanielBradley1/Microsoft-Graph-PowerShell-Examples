73ec609473f0acc93be13b273a0a495b0379a77b
Daniel Bradley <96792909+DanielBradley1@users.noreply.github.com>
2023-06-08 23:16:48 +0100

    Update User_Creation_Script.ps1

--- Chapter 7/User_Creation_Script.ps1
+++ Chapter 7/User_Creation_Script.ps1
@@ -1,9 +1,9 @@
-#Application ID: 7ee555a5-fd8b-4509-8d83-685f863
-#Tenant ID:      4e67cd72-f73a-42aa-a841-b8dd6ec3
-#Value:          Vbl8Q~qQxkoFrrt7rvzL8lTqo0I7lJUCEs
-#Secret ID:      929b644e-28a7-4457-9bc9-48c20
+#Application ID: 7ee555a5-fd8b-4509-8d83-685f863e9232
+#Tenant ID:      4e67cd72-f73a-42aa-a841-b8dd6ec328ca
+#Value:          Vbl8Q~qQxkoFrrt7rvzL8lTqo0I7lJUCEs~mxa~I
+#Secret ID:      929b644e-28a7-4457-9bc9-48c20d95b84c
 
-#New-MgUser -DisplayName ‘Daniel B -PasswordProfile $PassProfile -AccountEnabled -MailNickName ‘Daniel Bradley’ -UserPrincipalName dbra’
+#New-MgUser -DisplayName ‘Daniel Bradley’ -PasswordProfile $PassProfile -AccountEnabled -MailNickName ‘Daniel Bradley’ -UserPrincipalName dbradley@ourcloudnetwork.com’
 
 Param(
     [Parameter(Mandatory)]
@@ -12,12 +12,26 @@ Param(
 
     [Parameter(Mandatory)]
     [System.String]
-    ${LastName}
+    ${LastName},
+    
+    [Parameter(Mandatory)]
+    [System.String]
+    ${Domain}
 )
 
-$ApplicationId = "7ee555a5-fd8b-4509-8d83-685f863e"
-$SecuredPassword = "Vbl8Q~qQxkoFrrt7rvzL8lTqo0I7lJUCEs"
-$tenantID = "4e67cd72-f73a-42aa-a841-b8dd6ec"
+if ((($firstName | Measure-Object -Character).Characters -ge 64) -or $FirstName -notmatch '^[a-z]+$'){
+    Write-host "`n FirstName must be less that 64 characters and contain only characters from a-Z" -ForegroundColor Red
+}
+if ((($LastName | Measure-Object -Character).Characters -ge 64) -or $FirstName -notmatch '^[a-z]+$'){
+    Write-host "`n LastName must be less that 64 characters and contain only characters from a-Z" -ForegroundColor Red
+}
+if ((($Domain | Measure-Object -Character).Characters -ge 64) -or $FirstName -notmatch '^[a-z]+$'){
+    Write-host "`n FirstName must be less that 64 characters and contain only characters from a-Z" -ForegroundColor Red
+}
+
+$ApplicationId = "7ee555a5-fd8b-4509-8d83-685f863e9232"
+$SecuredPassword = "Vbl8Q~qQxkoFrrt7rvzL8lTqo0I7lJUCEs~mxa~I"
+$tenantID = "4e67cd72-f73a-42aa-a841-b8dd6ec328ca"
 
 $SecuredPasswordPassword = ConvertTo-SecureString `
 -String $SecuredPassword -AsPlainText -Force
@@ -26,4 +40,5 @@ $ClientSecretCredential = New-Object `
 -TypeName System.Management.Automation.PSCredential `
 -ArgumentList $ApplicationId, $SecuredPasswordPassword
 
-Connect-MgGraph -TenantId $tenantID -ClientSecretCredential $ClientSecretCredential
+#Connect-MgGraph -TenantId $tenantID -ClientSecretCredential $ClientSecretCredential
+
